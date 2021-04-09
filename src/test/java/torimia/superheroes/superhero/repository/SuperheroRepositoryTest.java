498650123ddd2bcefad13966e6064131c47a96af
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2021-04-09 12:52:53 +0300

    Disabled test which required docker

--- src/test/java/torimia/superheroes/superhero/repository/SuperheroRepositoryTest.java
+++ src/test/java/torimia/superheroes/superhero/repository/SuperheroRepositoryTest.java
@@ -1,6 +1,7 @@
 package torimia.superheroes.superhero.repository;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Transactional
 @SpringBootTest
+@Disabled
 class SuperheroRepositoryTest extends RepositoryDBForTests {
 
 //    @PersistenceContext
